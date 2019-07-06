60d12039c9bcfc8aa6a6c7d12cf464daed6b7a55
rob121 <rob121@gmail.com>
2019-07-06 08:47:23 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ cd cmd
 rice embed-go
 env GOOS=linux GOARCH=amd64 go build -o $1broadlinkgo-linux-amd64
 env GOOS=linux GOARCH=arm GOARM=5 go build -o $1broadlinkgo-linux-arm5-raspi
-env GOOS=windows GOARCH=amd64 go build -o $1broadlinkgo-windows-amd64
+env GOOS=windows GOARCH=amd64 go build -o $1broadlinkgo-windows-amd64.exe
 env GOOS=darwin GOARCH=amd64 go build -o $1broadlinkgo-darwin-amd64
